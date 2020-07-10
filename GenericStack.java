6ef79e0b42e200065b352a9c9888e76f0e36f764
Vinny Malhotra <vineet.malhotra125@gmail.com>
2020-07-09 23:27:58 -0400

    Arraylist implementation of a Stack with Generic type parameters

---
+++ GenericStack.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class GenericStack<T> {
+
+    //using an ArrayList under the hood for dynamic memory allocation, will increase 50% when capacity reached
+    private ArrayList<T> stackArr = new ArrayList<T>();
+    //store a pointer to the last element in the stack
+    private int top = 0;
+
+    /**
+     * add operation, amortized O(1)
+     * since only one element being added, it is constant time
+     */
+    public void push(T data){
+        stackArr.add(top, data);
+        top++;
+    }
+
+    /**
+     * remove operation cost is O(1) since the last element of the array is removed
+     * no elemental shifts will occur.. if the first element removed, then O(n) since all values shift left
+     */
+    public void pop(){
+        top--;
+        stackArr.remove(top);
+    }
+
+    /**
+     * O(1) since an Arraylist is an array under the hood, not looping through values
+     */
+    public T peek(){
+       return stackArr.get(top-1);
+    }
+
+    public String print(){
+        return stackArr.toString();
+    }
+}
